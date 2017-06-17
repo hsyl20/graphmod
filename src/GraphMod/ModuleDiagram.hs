20be1b30f52425bd5f69ff73397a582959332f95
Sylvain Henry <sylvain@haskus.fr>
2017-06-17 02:12:43 +0200

    Cleanup

--- src/GraphMod/ModuleDiagram.hs
+++ src/GraphMod/ModuleDiagram.hs
@@ -2,12 +2,13 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE PatternGuards #-}
 
 module GraphMod.ModuleDiagram where
 
 import Diagrams.Prelude
 --import Diagrams.Backend.SVG
---import Diagrams.Backend.Cairo
 import Diagrams.Backend.Rasterific
 import GraphMod.Utils
 
@@ -30,7 +31,7 @@ defaultOptions = DepOptions
 make_diagrams :: DepOptions -> [(ModName,[Import])] -> IO ()
 make_diagrams DepOptions{..} rawModuleImports = do
       --renderSVG' "deps.svg" (SVGOptions (mkWidth 3000) Nothing "" [] True) diag
-      --renderCairo "deps.png" (mkWidth 20000) diag
+      putStrLn (drawForest (fmap (fmap (\(_,t,_) -> joinModName t)) qtrees))
       renderRasterific "deps.png" (mkWidth 10000) diag
    where
       -- filterImport modName importName = not (  "GHC.Entity."      `isPrefixOf` importName
@@ -38,22 +39,19 @@ make_diagrams DepOptions{..} rawModuleImports = do
       --                                       || "GHC.Data."        `isPrefixOf` importName
       --                                       || "GHC.Config.Flags" `isPrefixOf` importName
       --                                       )
-      filterImport modName importName = "GHC.IR.Haskell.TypeChecker" `isPrefixOf` importName
-      --filterImport = const True
+      --filterImport modName importName = "GHC.IR.Haskell.TypeChecker" `isPrefixOf` importName
+      filterImport = \_ _ -> True
 
 
       -- First we have the list of the considered modules and their imports in
-      -- rawModuleImports
-
-      -- We allow module filtering with `filterModules`
+      -- `rawModuleImports`. We allow module filtering with `filterModules`.
       filteredModules = filter (uncurry filterModules) rawModuleImports
 
-      -- unqualified forest (name,value)
-      utrees = treeMergeAll [ treeMake qname imps | (qname,imps) <- rawModuleImports]
+      -- Now we can build a tree:
 
-      -- qualified forest (name,qualified name,value)
-      qtrees :: Forest (String,String,Maybe [Import])
-      qtrees = fmap treeQualify utrees
+      -- unqualified forest (name,value)
+      qtrees :: Forest (String,ModuleName,Maybe [Import])
+      qtrees = treeMergeAll [ treeMake qname imps | (qname,imps) <- filteredModules]
 
       -- show siblings dependencies
       siblingDeps = False
@@ -67,9 +65,9 @@ make_diagrams DepOptions{..} rawModuleImports = do
       diag :: QDiagram Rasterific V2 Float Any
       diag = moduleForest
       
-      moduleForest = hsep 2 (map diagModuleForest qtrees) 
+      moduleForest = hsep 2 (fmap diagModuleForest qtrees) 
                         # connectModules (extractForestChildren qtrees)
-                        # connectDeps [ (name,imp) | (name,is) <- rawModuleImports
+                        # connectDeps [ (name,imp) | (name,is) <- filteredModules
                                                    , imp <- is
                                                    ]
 
@@ -94,7 +92,7 @@ make_diagrams DepOptions{..} rawModuleImports = do
                                           ) m1 m2
 
       connectModules [] d         = d
-      connectModules ((a,b):cs) d = connectModules cs (d # moduleArrow a b)
+      connectModules ((a,b):cs) d = connectModules cs (d # moduleArrow (joinModName a) (joinModName b))
 
       connectDeps [] d                             = d
       connectDeps ((mname,Import imname itype):cs) d =
@@ -118,7 +116,7 @@ make_diagrams DepOptions{..} rawModuleImports = do
       filterModuleImports f (mname,imports) = (mname, filter (f mname) imports)
          
 
-      existingModules = fmap (joinModName . fst) rawModuleImports
+      existingModules = fmap (joinModName . fst) filteredModules
       doesModuleExist m = any (== m) existingModules
 
       extractNodeChildren node = [ (getQualifiedName node, getQualifiedName c) | c <- subForest node] ++ extractForestChildren (subForest node)
@@ -126,18 +124,21 @@ make_diagrams DepOptions{..} rawModuleImports = do
 
       getQualifiedName (Node (_,qname,_) _) = qname
 
-      diagModuleForest (Node v []) = drawModule v
-      diagModuleForest n@(Node v cs) =
-         vsep (2*sqrt (1 + fromIntegral (sumChildren n)))
-         [ drawModule v
-         , center (hsep 1 (map diagModuleForest cs))
-         ]
+      diagModuleForest n
+         | null (subForest n) = drawModule (rootLabel n)
+         | otherwise          = 
+            vsep (2*sqrt (1 + fromIntegral (sumChildren n)))
+            [ drawModule (rootLabel n)
+            , center (hsep 1 (map diagModuleForest (subForest n)))
+            ]
 
 
       drawModule (lbl,qname,_) =
          (text lbl 
-         <> rect 14 2 # lwG 0.2 # lc gray # if doesModuleExist qname then id else dashingG [0.3,0.3] 0
-         ) # named qname
+         <> rect 14 2 # lwG 0.2 # lc gray # if doesModuleExist (joinModName qname)
+                                                then id
+                                                else dashingG [0.3,0.3] 0
+         ) # named (joinModName qname)
 
       maxChildren :: Tree a -> Int
       maxChildren (Node _ []) = 1
@@ -156,28 +157,34 @@ treeQualify = go ""
          let q' = if q /= "" then q++"."++name else name
          in Node (name,q',v) (fmap (go q') cs)
       
-treeMake :: ModName -> a -> Tree (String,Maybe a)
-treeMake [] val     = Node ("{empty}",Just val) []
-treeMake [name] val = Node (name,Just val) []
-treeMake (q:qs) val = Node (q,Nothing) [ treeMake qs val]
+treeMake :: forall a. ModuleName -> a -> Tree (String,ModuleName,Maybe a)
+treeMake modName val = treeMake' [] modName
+   where
+      treeMake' _ []     = Node ("{empty}",modName,Just val) []
+      treeMake' _ [name] = Node (name     ,modName,Just val) []
+      treeMake' c (q:qs) = Node (q        ,c++[q] ,Nothing)
+                              [ treeMake' (c++[q]) qs]
 
-treeMerge :: Tree (String,Maybe a) -> Tree (String,Maybe a) -> Maybe (Tree (String,Maybe a))
+treeMerge :: Tree (String,ModuleName,Maybe a) -> Tree (String,ModuleName,Maybe a) -> Maybe (Tree (String,ModuleName,Maybe a))
 treeMerge t1 t2
-   | fst (rootLabel t1) == fst (rootLabel t2) =
-         let ret x = Just (Node (fst (rootLabel t1),x)
+   | (lbl1,qname,val1) <- rootLabel t1
+   , (lbl2,_,val2) <- rootLabel t2
+   , lbl1 == lbl2 =
+         let ret x = Just (Node (lbl1,qname,x)
                         (treeMergeAll (subForest t1 ++ subForest t2)))
          in
-         case (snd (rootLabel t1), snd (rootLabel t2)) of
+         case (val1,val2) of
             (Nothing,Nothing) -> ret Nothing
             (Just x ,Nothing) -> ret (Just x)
             (Nothing,Just x ) -> ret (Just x)
-            (Just _ ,Just _ ) -> error $ "Module defined twice: "++fst (rootLabel t1)
+            (Just _ ,Just _ ) -> error $ "Module defined twice: "++lbl1
    | otherwise = Nothing
 
-treeMergeAll :: Forest (String, Maybe a) -> Forest (String, Maybe a)
+treeMergeAll :: Forest (String, ModuleName, Maybe a) -> Forest (String, ModuleName, Maybe a)
 treeMergeAll [] = []
-treeMergeAll fs = foldr makeForest [] (sortOn (fst . rootLabel) fs)
+treeMergeAll fs = foldr makeForest [] (sortOn srt fs)
    where
+      srt = (\(x,_,_) -> x) . rootLabel
       makeForest t1 []      = [t1]
       makeForest t1 (t2:ts) = case treeMerge t1 t2 of
                                  Nothing -> t1:t2:ts
